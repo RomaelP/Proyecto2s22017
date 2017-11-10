3501886a8c98e9ee8573fd56e32947a03684f4fc
RomaelP <30426188+RomaelP@users.noreply.github.com>
2017-11-10 10:17:59 -0600

    avance Proyecto2 jsp

---
+++ Java/Proyecto2_EDD/src/java/Clases/reservaciones.java
@@ -0,0 +1,100 @@
+
+package Clases;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author USuario1
+ */
+public class reservaciones extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String tarjeta = request.getParameter("tarjeta");
+            String idfecha = calcularTotal.dia+calcularTotal.mes+calcularTotal.anio;
+            String monto = Integer.toString(calcularTotal.Total);
+            
+            /*out.print(coneccionServidor.Usuario+"   "+calcularTotal.Habitacion+"  "+tarjeta+"  "+monto+
+                    "  "+calcularTotal.anio+"  "+calcularTotal.mes+"  "+calcularTotal.dia+
+                    "  "+idfecha+"  "+calcularTotal.fechaEntrada+"  "+calcularTotal.fechaSalida);*/
+            
+            
+           
+            coneccionServidor multiples = new coneccionServidor();
+            String res = multiples.agregarReservacion(coneccionServidor.Usuario, 
+                   calcularTotal.Habitacion,
+                   tarjeta,
+                   monto,
+                   calcularTotal.anio, 
+                   calcularTotal.mes, 
+                   calcularTotal.dia, 
+                   idfecha, 
+                   calcularTotal.fechaEntrada, 
+                   calcularTotal.fechaSalida);
+            
+            System.out.print(res);
+        
+        //request.setAttribute("habitaciones", coneccionServidor.cadenaHabitaciones);
+        request.getRequestDispatcher("inicioUsuario.jsp").forward(request, response);
+        }
+        //limpiarVariables();
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    public void limpiarVariables() {
+        calcularTotal.Habitacion = "";
+        calcularTotal.anio = ""; 
+                   calcularTotal.mes = ""; 
+                   calcularTotal.dia = ""; 
+                   calcularTotal.fechaEntrada = ""; 
+                   calcularTotal.fechaSalida = "";
+    }
+
+}
